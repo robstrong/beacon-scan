62f14dd756bd0693ebc1898b07cdebd15891dc35
Rob Strong <robert.a.strong@gmail.com>
2018-07-01 20:26:12 -0400

    fix visibility bug

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"log"
 	"math"
+
+	"github.com/eclipse/paho.mqtt.golang"
 )
 
 var configPath = flag.String("config", "config.toml", "path to config file")
@@ -20,18 +22,8 @@ func main() {
 
 	//register each UUID from config
 	for _, u := range c.UUIDs {
-		s.AddSubscriber(func(d BeaconData) {
-			if d.UUID != u.UUID {
-				return
-			}
-			payload := newPayload(u.DeviceID, u.Name, calculateDistance(d.RSSI, d.TxPower))
-			log.Printf("publishing to %s: %s\n", u.Topic, payload)
-			t := mq.Publish(u.Topic, 0, false, payload)
-			t.Wait()
-			if err := t.Error(); err != nil {
-				log.Printf("error publishing: %v", err)
-			}
-		})
+		log.Printf("adding %s", u.UUID)
+		s.AddSubscriber(handleBeaconAdvert(u, mq))
 	}
 
 	//start listening
@@ -50,4 +42,19 @@ func calculateDistance(rssi int, txPower int) float64 {
 		return math.Pow(ratio, 10)
 	}
 	return (0.89976)*math.Pow(ratio, 7.7095) + 0.111
+}
+
+func handleBeaconAdvert(u TrackedUUID, mq mqtt.Client) BeaconDiscovered {
+	return func(d BeaconData) {
+		if d.UUID != u.UUID {
+			return
+		}
+		payload := newPayload(u.DeviceID, u.Name, calculateDistance(d.RSSI, d.TxPower))
+		log.Printf("publishing to %s: %s\n", u.Topic, payload)
+		t := mq.Publish(u.Topic, 0, false, payload)
+		t.Wait()
+		if err := t.Error(); err != nil {
+			log.Printf("error publishing: %v", err)
+		}
+	}
 }
